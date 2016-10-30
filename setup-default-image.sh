e731a9b3d9537cce9ffd809182fb2ddeecad41ab
Marco Schwochow <ms.dev@schwochow.com>
2016-10-30 18:33:05 +0100

    alpha-0.1

--- setup-default-image.sh
+++ setup-default-image.sh
@@ -4,14 +4,14 @@
 cd /home/admin
 
 # Update/Upgrade apt-get
-sudo apt-get clean && sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get clean && sudo apt-get --force-yes update && sudo apt-get --force-yes dist-upgrade
 
 # Install all dependencies for Nominatim (Debian 8)
 # http://wiki.openstreetmap.org/wiki/Nominatim/Installation
-sudo apt-get install build-essential libxml2-dev libpq-dev libbz2-dev libtool automake libproj-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libexpat-dev gcc proj-bin libgeos-c1 libgeos++-dev libexpat-dev php5 php-pear php5-pgsql php5-json php-db libapache2-mod-php5 postgresql postgis postgresql-contrib postgresql-9.4-postgis-2.1 postgresql-server-dev-9.4 wget
+sudo apt-get install build-essential libxml2-dev libpq-dev libbz2-dev libtool automake libproj-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libexpat-dev gcc proj-bin libgeos-c1 libgeos++-dev libexpat-dev php5 php-pear php5-pgsql php5-json php-db libapache2-mod-php5 postgresql postgis postgresql-contrib postgresql-9.4-postgis-2.1 postgresql-server-dev-9.4 wget -y
 
 # Install osmosis
-sudo apt-get install osmosis
+sudo apt-get install osmosis -y
 
 # Install Nominatim-2.5.1
 wget http://www.nominatim.org/release/Nominatim-2.5.1.tar.bz2
